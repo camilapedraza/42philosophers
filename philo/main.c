ad3b90e25a4c6edc87eb82d632e08fb3044233f0
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2026-02-10 21:08:53 +0100

    working fork and eating routine, plus monitor thread - norminette passing

--- philo/main.c
+++ philo/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.c                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:44:57 by mpedraza          #+#    #+#             */
-/*   Updated: 2026/02/09 16:28:54 by mpedraza         ###   ########.fr       */
+/*   Updated: 2026/02/10 21:02:34 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,78 +20,74 @@ pthread_mutex_unlock */
 
 #include "philosophers.h"
 
-int	should_sim_stop(t_sim *sim)
+void	exit_simulation(t_sim *sim, t_philo *philosophers, pthread_t *threads)
 {
-	int	value;
+	int	i;
 
-	pthread_mutex_lock(&sim->stop_mutex);
-	value = sim->stop;
-	pthread_mutex_unlock(&sim->stop_mutex);
-	return (value);
-}
-
-void	*routine(void *arg)
-{
-	t_philo	*philosopher;
-
-	philosopher = (t_philo *)arg;
-	print_status(philosopher, "started");
-	while (!should_sim_stop(philosopher->sim))
-	{
-		take_forks(philosopher);
-		print_status(philosopher, EAT);
-		usleep(200000);
-		release_forks(philosopher);
-		usleep(200000);
-	}
-	print_status(philosopher, "stopped");
-	return (NULL);
+	i = 0;
+	while (i < sim->nb_philos)
+		pthread_mutex_destroy(&sim->forks[i++]);
+	pthread_mutex_destroy(&sim->stop_mutex);
+	pthread_mutex_destroy(&sim->print_mutex);
+	free(sim->forks);
+	free(philosophers);
+	free(threads);
 }
 
 int	init_threads(t_sim *sim, t_philo *philosophers, pthread_t **threads)
 {
-	int i;
+	int	i;
 
-	*threads = malloc(sizeof(pthread_t) * sim->nb_philosophers);
+	*threads = malloc(sizeof(pthread_t) * sim->nb_philos);
 	if (!*threads)
 		return (FAILURE);
 	i = 0;
-	while (i < sim->nb_philosophers)
+	while (i < sim->nb_philos)
 	{
-		pthread_create(&(*threads)[i], NULL, routine, &philosophers[i]);
+		pthread_create(&(*threads)[i], NULL, philo_routine, &philosophers[i]);
 		i++;
 	}
 	return (SUCCESS);
 }
+
 int	init_philosophers(t_sim *sim, t_philo **philosophers)
 {
-	int i;
+	int	i;
 
-	*philosophers = malloc(sizeof(t_philo) * sim->nb_philosophers);
+	*philosophers = malloc(sizeof(t_philo) * sim->nb_philos);
 	if (!*philosophers)
 		return (FAILURE);
 	i = 0;
-	while (i < sim->nb_philosophers)
+	while (i < sim->nb_philos)
 	{
-		(*philosophers)[i].id = i + 1;
 		(*philosophers)[i].sim = sim;
+		(*philosophers)[i].id = i + 1;
+		(*philosophers)[i].last_meal_time = sim->start_time;
+		(*philosophers)[i].meals_eaten = 0;
 		(*philosophers)[i].fork_left = &sim->forks[i];
-		(*philosophers)[i].fork_right = &sim->forks[(i + 1) % sim->nb_philosophers];
+		(*philosophers)[i].fork_right = &sim->forks[(i + 1) % sim->nb_philos];
+		pthread_mutex_init(&(*philosophers)[i].meal_mutex, NULL);
 		i++;
 	}
 	return (SUCCESS);
 }
 
+// TODO time_to_eat needs to be dynamic based on parameters
+// TODO time_to_die needs to be dynamic based on parameters
 int	init_simulation(t_sim *sim, int n)
 {
-	int i;
+	int	i;
 
+	if (n <= 0)
+		return (FAILURE);
 	sim->forks = malloc(sizeof(pthread_mutex_t) * n);
 	if (!sim->forks)
 		return (FAILURE);
 	sim->start_time = get_time_ms();
-	sim->nb_philosophers = n;
-	sim->stop = 0;
+	sim->time_to_eat = 200;
+	sim->time_to_die = 500;
+	sim->nb_philos = n;
+	sim->stop = false;
 	i = 0;
 	while (i < n)
 		pthread_mutex_init(&sim->forks[i++], NULL);
@@ -103,37 +99,25 @@ int	init_simulation(t_sim *sim, int n)
 // TODO guard clause for ac < 4
 int	main(int ac, char **av)
 {
-	int			n;
 	int			i;
 	pthread_t	*threads;
 	t_philo		*philosophers;
 	t_sim		sim;
+	pthread_t	monitor;
 
 	if (ac < 2)
 		return (1);
-	n = ft_atoi(av[1]);
-	if (n <= 0)
-		return (1);
-	if (!init_simulation(&sim, n))
+	if (!init_simulation(&sim, ft_atoi(av[1])))
 		exit(EXIT_FAILURE);
 	if (!init_philosophers(&sim, &philosophers))
 		exit(EXIT_FAILURE);
 	if (!init_threads(&sim, philosophers, &threads))
 		exit(EXIT_FAILURE);
-	sleep(1);
-	pthread_mutex_lock(&sim.stop_mutex);
-	sim.stop = 1;
-	pthread_mutex_unlock(&sim.stop_mutex);
+	pthread_create(&monitor, NULL, monitor_routine, philosophers);
+	pthread_detach(monitor);
 	i = 0;
-	while (i < n)
+	while (i < sim.nb_philos)
 		pthread_join(threads[i++], NULL);
-	i = 0;
-	while (i < n)
-		pthread_mutex_destroy(&sim.forks[i++]);
-	pthread_mutex_destroy(&sim.stop_mutex);
-	pthread_mutex_destroy(&sim.print_mutex);
-	free(sim.forks);
-	free(philosophers);
-	free(threads);
+	exit_simulation(&sim, philosophers, threads);
 	return (0);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,59 +6,79 @@
 /*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:44:23 by mpedraza          #+#    #+#             */
-/*   Updated: 2026/02/09 16:02:25 by mpedraza         ###   ########.fr       */
+/*   Updated: 2026/02/10 21:15:17 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <stdbool.h>
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
 # include <unistd.h>
 
-/* STATUS MESSAGES */
-
-# define FORK	"has taken a fork"
-# define EAT	"is eating"
-# define SLEEP	"is sleeping"
-# define THINK	"is thinking"
-# define DEATH	"died"
+/* FUNCTION RETURN MESSAGES */
 
 # define FAILURE 0
 # define SUCCESS 1
 
+/* PHILOSOPHER STATUS */
+
+typedef enum e_status
+{
+	TAKEN_FORK,
+	EATING,
+	SLEEPING,
+	THINKING,
+	DEAD
+}	t_status;
+
 typedef struct s_simulation
 {
 	long			start_time;
-	int				stop;
-	int				nb_philosophers;
+	long			time_to_eat;
+	long			time_to_die;
+	bool			stop;
+	int				nb_philos;
 
-	pthread_mutex_t *forks;
+	pthread_mutex_t	*forks;
 	pthread_mutex_t	stop_mutex;
-	pthread_mutex_t print_mutex;
+	pthread_mutex_t	print_mutex;
 }	t_sim;
 
 typedef struct s_philosopher
 {
-	int		id;
+	int				id;
+	int				meals_eaten;
+	long			last_meal_time;
 
-	pthread_mutex_t *fork_left;
-	pthread_mutex_t *fork_right;
+	pthread_mutex_t	*fork_left;
+	pthread_mutex_t	*fork_right;
+	pthread_mutex_t	meal_mutex;
 
-	t_sim	*sim;
+	t_sim			*sim;
 }	t_philo;
 
-/* PHILOSOPHER TASKS */
+/* SIMULATION MONITOR */
+void	*monitor_routine(void *arg);
+bool	should_sim_stop(t_sim *sim);
+
+/* PHILOSOPHER ROUTINE */
+void	*philo_routine(void *arg);
 void	take_forks(t_philo *philosopher);
 void	release_forks(t_philo *philosopher);
+void	eat(t_philo *philosopher);
+
+/*	PRINT MODULE */
+void	print_status(t_philo *philosopher, t_status status);
 
 /* UTILITIES */
+long	to_usec(long time);
 long	get_time_ms(void);
 long	timestamp_ms(t_sim *sim);
-void	print_status(t_philo *philosopher, const char *status);
 int		ft_atoi(const char *nptr);
 
 #endif
